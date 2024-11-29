c1020fba6561befced57fd2dc1e5076924fdb802
Moksha K <110051904+Moksha-Kachhia@users.noreply.github.com>
2024-11-29 13:30:57 -0500

    Made the LaunchPageActivity to fulfil project requirement of having users presented with the option to login or sign up at launch, and edited the sign up activity ui to match login better.

---
+++ App/app/src/main/java/com/example/planetzeapp/LaunchPageActivity.java
@@ -0,0 +1,47 @@
+package com.example.planetzeapp;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class LaunchPageActivity extends AppCompatActivity {
+    private Button loginButton, signupButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_launch_page);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        loginButton = findViewById(R.id.login_button);
+        signupButton = findViewById(R.id.signup_button);
+
+        loginButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(LaunchPageActivity.this, LoginActivity.class));
+            }
+        });
+
+        signupButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(LaunchPageActivity.this, LoginActivity.class));
+            }
+        });
+    }
+
+}
