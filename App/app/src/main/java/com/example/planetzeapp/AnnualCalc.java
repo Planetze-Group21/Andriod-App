889bf06b907bb2f52a942c925e96dee4f2469876
habibahafez <habibawhafez@gmail.com>
2024-11-30 15:41:26 -0500

    adding small feature

--- App/app/src/main/java/com/example/planetzeapp/AnnualCalc.java
+++ App/app/src/main/java/com/example/planetzeapp/AnnualCalc.java
@@ -11,12 +11,15 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.util.Patterns;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.activity.EdgeToEdge;
 import androidx.core.graphics.Insets;
@@ -47,8 +50,25 @@ public class AnnualCalc extends Fragment {
          System.err.println("No user is signed in. Redirecting to login...");
          userRef = null;
       }
+   }
+
+   @Override
+   public void onCreate(@Nullable Bundle savedInstanceState) {
+      super.onCreate(savedInstanceState);
+      Log.d("AnnualCalc", "onCreate called.");
+
       executeAllEmissionsCalculations();
    }
+
+   @Override
+   public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+      Log.d("AnnualCalc", "onCreateView called.");
+
+      View view = inflater.inflate(R.layout.fragment_annual_calc, container, false);
+
+      return view;
+   }
+
 
    public void executeAllEmissionsCalculations() {
       Annual_Home_emissions(() -> {
